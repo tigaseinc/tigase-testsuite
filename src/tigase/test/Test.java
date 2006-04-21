6db8ecce60a34b31c808e1b28b3dd8afc0528e51
Artur Hefczyc <kobit@tigase.org>
2006-04-21 07:15:24 +0000

    Corrected thread synchronization issues
    git-svn-id: file:///home/svn/repos/tigase-testsuite/trunk@11 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/tigase/test/Test.java
+++ src/tigase/test/Test.java
@@ -181,18 +181,19 @@ public class Test {
 
   private boolean runTest(List<TestIfc> suite, Params test_params) {
     boolean daemon = test_params.containsKey("-daemon");
+		long socket_wait = test_params.get("-socket-wait", 5000;
     DaemonTest dt = new DaemonTest(suite, test_params);
     if (daemon) {
       runThread(dt);
       synchronized(dt) {
-        try { dt.wait(5000); } catch (InterruptedException e) { }
+        try { dt.wait(socket_wait); } catch (InterruptedException e) { }
       }
-      if (!dt.isAuthorized()) {
-        runThread(dt);
-        synchronized(dt) {
-          try { dt.wait(5000); } catch (InterruptedException e) { }
-        }
-      } // end of if (!dt.isAuthorized())
+//       if (!dt.isAuthorized()) {
+//         runThread(dt);
+//         synchronized(dt) {
+//           try { dt.wait(5000); } catch (InterruptedException e) { }
+//         }
+//       } // end of if (!dt.isAuthorized())
       return true;
     } // end of if (daemon)
     else {
