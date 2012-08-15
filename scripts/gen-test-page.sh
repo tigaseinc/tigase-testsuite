3e9e4ee806a07f0c8102074e5c6913f222c4f95a
Artur Hefczyc <kobit@tigase.org>
2012-08-15 22:20:44 +0000

    Helper scripts to update the server code and generate index.html with all tests reports
    git-svn-id: file:///home/svn/repos/tigase-testsuite/trunk@243 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ scripts/gen-test-page.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+TEST_REPORTS="mysql pgsql derby"
+TEST_TYPES="func lmem"
+declare -A MAIN_FILE
+MAIN_FILE=( [func]="functional-tests.html" [lmem]="low-memory-tests.html" )
+
+function echoindex() {
+
+  echo "$1" >> ${INDEX_DIR}/index.html
+
+}
+
+function gettesttime() {
+
+  testtime=`grep "Total test time" $1 | sed -e "s/[^0-9]\+\([0-9]\+\)[^0-9]\+\([0-9]\+\)[^0-9]\+\([0-9]\+\)[^0-9]\+\([0-9]\+\).*/\1:\2:\3:\4/"`
+  echo "${testtime}"
+
+}
+
+INDEX_START="index-start.txt"
+INDEX_END="index-end.txt"
+SRC_DIR="scripts/tests-page"
+INDEX_DIR="/home/tigase/public_html"
+REPORTS_LOC="files/static/tests"
+
+ALL_REPORTS_DIRS=`ls -1 ${INDEX_DIR}/${REPORTS_LOC} | sort -r`
+
+cat ${SRC_DIR}/${INDEX_START} > ${INDEX_DIR}/index.html
+
+for tt in ${TEST_TYPES} ; do
+
+  cat ${SRC_DIR}/${tt}-start.txt >> ${INDEX_DIR}/index.html
+
+  cat ${SRC_DIR}/headers-start.txt >> ${INDEX_DIR}/index.html
+
+  for tr in ${TEST_REPORTS} ; do
+
+    cat ${SRC_DIR}/${tr}-start.txt >> ${INDEX_DIR}/index.html
+
+  done
+
+  cat ${SRC_DIR}/headers-end.txt >> ${INDEX_DIR}/index.html
+
+  for ard in ${ALL_REPORTS_DIRS} ; do
+
+      IS_TEST=""
+
+      for tr in ${TEST_REPORTS} ; do
+
+        MFILE1="${INDEX_DIR}/${REPORTS_LOC}/${ard}/${tt}/${tr}/"${MAIN_FILE[${tt}]}
+        MFILE2="${INDEX_DIR}/${REPORTS_LOC}/${ard}/${tt}/${tr}-auth/functional-tests.html"
+        MFILE3="${INDEX_DIR}/${REPORTS_LOC}/${ard}/${tt}/${tr}-custom/functional-tests.html"
+        
+        if [ -f ${MFILE1} ] || [ -f ${MFILE2} ] || [ -f ${MFILE3} ] ; then
+          IS_TEST="${IS_TEST}${tr}"
+        fi
+
+      done      
+
+      if [ ! -z ${IS_TEST} ] ; then
+
+      echoindex "<tr><th>${ard}</th>"
+
+      bgcolor="#90FF90"
+
+      for tr in ${TEST_REPORTS} ; do
+
+        MFILE1="${REPORTS_LOC}/${ard}/${tt}/${tr}/"${MAIN_FILE[${tt}]}
+        MFILE2="${REPORTS_LOC}/${ard}/${tt}/${tr}-auth/"${MAIN_FILE[${tt}]}
+	MFILE3="${REPORTS_LOC}/${ard}/${tt}/${tr}-custom/"${MAIN_FILE[${tt}]}
+
+        MFILES="${MFILE1} ${MFILE2} ${MFILE3}"
+
+        for mf in ${MFILES} ; do
+
+          if [ -f ${INDEX_DIR}/${mf} ] ; then
+            bgcolor="#90FF90"
+            if grep "FAILURE" ${INDEX_DIR}/${mf} > /dev/null ; then
+              bgcolor="FF9090"
+            fi
+
+            ttime=`gettesttime ${INDEX_DIR}/${mf}`
+            echoindex "<td class=\"rtecenter\" bgcolor=\"${bgcolor}\"><a href=\"${mf}\">${ttime}</a></td>"
+          else
+            echoindex "<td class=\"rtecenter\"> --- </td>"
+          fi
+
+        done
+
+      done
+
+      echoindex "</tr>"
+
+      fi
+
+  done
+
+  cat ${SRC_DIR}/table-end.txt >> ${INDEX_DIR}/index.html
+
+done
+
+cat ${SRC_DIR}/${INDEX_END} >> ${INDEX_DIR}/index.html
---
+++ scripts/prepare-tests.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#set -x
+
+PROJECTS_DIR="/home/tigase/projects"
+TMP_DIR="/tmp"
+
+SRV_PACKAGES="xmltools utils extras muc pubsub server"
+
+function build_xmltools() {
+  ant clean jar-dist &> ${TMP_DIR}/xmltools-build.txt
+  echo jars/tigase-xmltools.jar
+}
+
+function build_utils() {
+  cp -f ../xmltools/jars/tigase-xmltools.jar libs/
+  ant clean jar-dist &> ${TMP_DIR}/utils-build.txt
+  echo jars/tigase-utils.jar
+}
+
+function build_server() {
+  ant clean jar-dist &> ${TMP_DIR}/xmltools-build.txt
+  echo ""
+}
+
+function build_maven() {
+  mvn clean package &> ${TMP_DIR}/$1-build.txt
+  echo `find target -name "tigase-*"`
+}
+
+function build_extras() {
+  build_maven extras
+}
+
+function build_muc() {
+  build_maven muc
+}
+
+function build_pubsub() {
+  build_maven pubsub
+}
+
+MY_DIR=`pwd`
+
+cd ${PROJECTS_DIR}
+
+# Build all server packages
+for p in $SRV_PACKAGES ; do
+  echo "Building package: $p"
+  cd $p
+  svn up
+  JAR=`build_$p`
+  echo "jar file: $JAR"
+  if [ ! -z "${JAR}" ] ; then
+    cp -f $JAR ../server/libs/tigase-$p.jar
+  fi
+  cd ..
+done
+
+echo "Updating TTS"
+
+cd testsuite
+svn up
+ant clean jar &> ../testsuite-build.txt
+cd ..
+
+cd ${MY_DIR}
