3c404de94ef6e092b2316869dcc637c040bd1e03
Wojciech Kapcia <wojciech.kapcia@tigase.org>
2014-01-27 11:28:14 +0100

    change script preparing test environment

--- scripts/prepare-tests.sh
+++ scripts/prepare-tests.sh
@@ -5,73 +5,65 @@
 if [ "${PROJECTS_DIR}" == "" ] ; then
   PROJECTS_DIR="/home/tigase/projects"
 fi
-TMP_DIR="/tmp"
 
-SRV_PACKAGES="xmltools utils extras muc pubsub server"
+echo "Project home directory: ${PROJECTS_DIR}"
 
-function build_xmltools() {
-  ant clean jar-dist &> ${TMP_DIR}/xmltools-build.txt
-  echo jars/tigase-xmltools.jar
-}
-
-function build_utils() {
-  cp -f ../xmltools/jars/tigase-xmltools.jar libs/
-  ant clean jar-dist &> ${TMP_DIR}/utils-build.txt
-  echo jars/tigase-utils.jar
-}
-
-function build_server() {
-  ant clean jar-dist &> ${TMP_DIR}/server-build.txt
-  echo ""
-}
-
-function build_maven() {
-  mvn clean package &> ${TMP_DIR}/$1-build.txt
-  echo `find target -name "tigase-*"`
-}
-
-function build_extras() {
-  build_maven extras
-}
-
-function build_muc() {
-  build_maven muc
-}
-
-function build_pubsub() {
-  build_maven pubsub
-}
+if [ "${1}" == "" ] ; then
+	TEST_VERSION="master"
+else
+	TEST_VERSION="${1}"
+fi
 
 MY_DIR=`pwd`
 
 cd ${PROJECTS_DIR}
 
-# Build all server packages
-for p in $SRV_PACKAGES ; do
-  echo "Building package: $p"
-  cd $p
-  mkdir -p libs jars
-  git pull
-  JAR=`build_$p`
-  echo "jar file: $JAR"
-  if [ ! -z "${JAR}" ] ; then
-    cp -f $JAR ../server/libs/tigase-$p.jar
-  fi
-  cd ..
-done
+#get or update sources
+echo "Preparing server version: ${TEST_VERSION}"
+if [ ! -e tigase-server ] ; then
+	git clone https://repository.tigase.org/git/tigase-server.git
+else
+	cd tigase-server
+	git fetch origin
+	cd ..
+fi
 
-# Copy over admin scripts
-mkdir -p server/scripts/admin/
-cp -f server/src/main/groovy/tigase/admin/*.groovy server/scripts/admin/
-
-echo "Updating TTS"
-
-cd testsuite
-mkdir -p libs jars
-cp ../xmltools/jars/tigase-xmltools.jar libs/
-cp ../utils/jars/tigase-utils.jar libs/
-git pull
-ant clean jar &> ../testsuite-build.txt
+#prepare specific version
+cd tigase-server
+git reset --hard -q origin/master
+git checkout -q -f ${TEST_VERSION}
 cd ..
+
+echo "Preparing tigase testsuite"
+if [ ! -e tigase-testsuite ] ; then
+        git clone https://repository.tigase.org/git/tigase-testsuite.git
+else
+		cd tigase-testsuite
+        git pull
+        git checkout master
+		cd ..
+fi
+
+#clean previous builds
+echo "Cleaning previous builds"
+rm -rf tigase-server/pack/*
+rm -rf tigase-server/target/*
+
+#create packages
+echo "Creating server package"
+cd tigase-server
+mvn -f modules/master/pom.xml  clean package
+cd ..
+
+if [ -e tigase-server-dist ] ; then
+	rm -rf tigase-server-dist || exit -1
+fi
+
+mkdir tigase-server-dist || exit -1
+
+tar --strip-components=1 -xf tigase-server/pack/tigase-server-*-dist-max.tar.gz -C tigase-server-dist
+
+cd tigase-testsuite
+mvn clean package
 
 cd ${MY_DIR}
