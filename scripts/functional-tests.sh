96fbd1603c35c395c0e1de7bfbb7b22b0cefcdf0
Wojciech Kapcia <wojtek@tigase.org>
2019-01-21 13:46:57 -0300

    #8483 update licence headers

--- scripts/functional-tests.sh
+++ scripts/functional-tests.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
-##
-##  Tigase XMPP/Jabber Test Suite
-##  Copyright (C) 2004-2009 "Artur Hefczyc" <artur.hefczyc@tigase.org>
-##
-##  This program is free software: you can redistribute it and/or modify
-##  it under the terms of the GNU General Public License as published by
-##  the Free Software Foundation, either version 3 of the License.
-##
-##  This program is distributed in the hope that it will be useful,
-##  but WITHOUT ANY WARRANTY; without even the implied warranty of
-##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##  GNU General Public License for more details.
-##
-##  You should have received a copy of the GNU General Public License
-##  along with this program. Look for COPYING file in the top folder.
-##  If not, see http://www.gnu.org/licenses/.
-##
-##  $Rev: $
-##  Last modified by $Author: $
-##  $Date: $
-##
+#
+# Tigase Test Suite - Automated testing framework for Tigase Jabber/XMPP Server.
+# Copyright (C) 2005 Tigase, Inc. (office@tigase.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. Look for COPYING file in the top folder.
+# If not, see http://www.gnu.org/licenses/.
+#
 
 java -Dcom.sun.management.jmxremote -Xmx100M -cp jars/tigase-xmpp-testsuite.jar:libs/tigase-utils.jar:libs/tigase-xmltools.jar tigase.test.TestSuite -script scripts/all-xmpp-tests.xmpt $*
